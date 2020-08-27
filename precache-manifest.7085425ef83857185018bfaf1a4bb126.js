a543dd81877557211de8e714515349c27b4e7c74
Nilesh Pant <nileshpant6969@gmail.com>
2020-08-27 18:52:51 +0530

    Updates

---
+++ precache-manifest.7085425ef83857185018bfaf1a4bb126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fb7f03a29b000aa84a90111dd2382e",
+    "url": "/dsc-website/index.html"
+  },
+  {
+    "revision": "e98b050d75276e7826b9",
+    "url": "/dsc-website/static/css/main.5b8a9864.chunk.css"
+  },
+  {
+    "revision": "ed9aa22dc1bf0a4dd8e4",
+    "url": "/dsc-website/static/js/2.4d97316c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dsc-website/static/js/2.4d97316c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e98b050d75276e7826b9",
+    "url": "/dsc-website/static/js/main.81e2dde3.chunk.js"
+  },
+  {
+    "revision": "5283d32be8c0d2125c14",
+    "url": "/dsc-website/static/js/runtime-main.e6a3fe2b.js"
+  }
+]);
