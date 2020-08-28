f7bc3dab6c5d138839e352382d01408757ae7754
Nilesh Pant <nileshpant6969@gmail.com>
2020-08-28 17:05:01 +0530

    Updates

---
+++ precache-manifest.6ca232ff0883c5da3f449597153c0cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef196f0d79ab75ea31c582b56e3868e5",
+    "url": "/dsc-website/index.html"
+  },
+  {
+    "revision": "f7a2e5d3b66fef5fe300",
+    "url": "/dsc-website/static/css/main.b8c044b9.chunk.css"
+  },
+  {
+    "revision": "48b8c049f66ed7282e8b",
+    "url": "/dsc-website/static/js/2.48215f90.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dsc-website/static/js/2.48215f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a2e5d3b66fef5fe300",
+    "url": "/dsc-website/static/js/main.2a456970.chunk.js"
+  },
+  {
+    "revision": "5283d32be8c0d2125c14",
+    "url": "/dsc-website/static/js/runtime-main.e6a3fe2b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dsc-website/precache-manifest.e9dab4d2137d6db8388ac1f64bf94858.js"
+  "/dsc-website/precache-manifest.6ca232ff0883c5da3f449597153c0cb8.js"
 );
 
 self.addEventListener('message', (event) => {
